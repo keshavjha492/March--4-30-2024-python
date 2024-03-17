1724eb9710a295fffa1dd938b3b4d529b405aa5a
keshavjha492 <keshavjha492@gmail.com>
2024-03-17 17:31:45 +0545

    new

---
+++ Day9/1_nested_if.py
@@ -0,0 +1,14 @@
+# "Nested if" is also possible in python
+
+
+a = 1
+b = 2
+c = 3
+
+if c > a:
+    if c > b:
+        print("c is the greatest")
+    else:
+        print("c is not the greatest")
+else:
+    print("a is greater than c")
