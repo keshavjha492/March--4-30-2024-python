0544b3135adf4d8a4262bcdfc4c3ebf07efe1359
keshavjha492 <keshavjha492@gmail.com>
2024-03-22 21:04:13 +0545

    assignment

--- Day10/4_dict_comprehension.py
+++ Day10/4_dict_comprehension.py
@@ -32,3 +32,11 @@ values = ["Jon", 30, "KTM"]
 
 data = zip(keys, values)
 print(dict(data))
+
+
+#tuple comprehension
+data= (i for i in range(5)) #it looks like tuple compression but it actually is a generator expression
+print(data)  # generator_expression
+
+for i in data:
+    print(i)
